bc3902ce117dd56e4783eebe6ecc94b4695f9043
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-09 07:33:21 -0600

    Getting Started

---
+++ ResponsiveGridViewSwiftUI/ResponsiveGridViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveGridViewSwiftUIApp.swift
+//  ResponsiveGridViewSwiftUI
+//
+//  Created by Marco Alonso Rodriguez on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveGridViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
