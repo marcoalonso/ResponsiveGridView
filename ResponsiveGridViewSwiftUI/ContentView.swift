94ac7529f6166f9dc27653dae50204ea62cac903
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-09 07:43:39 -0600

    VGrid finished with adaptive columns

--- ResponsiveGridViewSwiftUI/ContentView.swift
+++ ResponsiveGridViewSwiftUI/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var data : [Int] = Array(1...49)
+    private let colors: [Color] = [.red, .gray, .green, .yellow, .blue, .pink, .brown]
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 15) {
+                    ForEach(data, id: \.self) { number in
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 150, height: 150)
+                                .foregroundColor(colors[number%7])
+                                .cornerRadius(25)
+                            
+                            Text("\(number)")
+                                .font(.system(size: 80, weight: .medium, design: .rounded))
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Grid Example")
         }
-        .padding()
     }
 }
 
