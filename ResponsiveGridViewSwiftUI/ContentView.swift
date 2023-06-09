1629d98107b340587dca6cf9aa3a819552673ae9
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-09 07:49:13 -0600

    GridItem flexible

--- ResponsiveGridViewSwiftUI/ContentView.swift
+++ ResponsiveGridViewSwiftUI/ContentView.swift
@@ -11,18 +11,27 @@ struct ContentView: View {
     private var data : [Int] = Array(1...49)
     private let colors: [Color] = [.red, .gray, .green, .yellow, .blue, .pink, .brown]
     
+    //Option 1,
     private let adaptiveColumns = [
         GridItem(.adaptive(minimum: 150))
+    ]
+    
+    //Option 2
+    private let numberOfColumns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
     ]
     
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: adaptiveColumns, spacing: 15) {
+                LazyVGrid(columns: numberOfColumns, spacing: 15) {
                     ForEach(data, id: \.self) { number in
                         ZStack {
                             Rectangle()
-                                .frame(width: 150, height: 150)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                                .frame(width: 150, height: 150)
                                 .foregroundColor(colors[number%7])
                                 .cornerRadius(25)
                             
